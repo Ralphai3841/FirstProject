0a567178016ea1184616b725be7c0c106ff64d53
ralpd <ralpd@LAPTOP-FB9086OP>
2023-09-21 05:18:24 +0300

    Add project files.

---
+++ FirstProject/Controller/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller   //controller
+    {
+        public string Index()   // action
+        {
+            return "Uygulama çalışıyor.. ";
+        }
+        public string Test()
+        {
+            return "bu test action dır";
+        }
+        public IActionResult Selamla()       // IActionResult tipinde döndük
+        {
+            ViewResult result = View();
+            return result;
+
+        }
+        public IActionResult Selamla2()       
+        {
+            ViewResult result = View("Test2");
+            return result;
+
+        }
+
+    }
+}
---
+++ FirstProject/Controller/UserController.cs
@@ -0,0 +1,27 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            User user = new User();
+            user.ID = 1;
+            user.FirstName = "Rasim";
+            user.LastName = "Demirci";
+            return View();
+        }
+
+        [NonAction]
+        public string GetFullName(User user)   // geri döndürmek
+        {
+            return user.FirstName + " " + user.LastName;
+        }
+    }
+}
